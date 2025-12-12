3e759754031eb3842c490340d3cfa51093c0c75c
Astral <drorl1590@gmail.com>
2025-12-12 14:43:10 +0000

    added SG to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,63 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "vm" {
-  ami           = "ami-03873003744ddc492"
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get latest Ubuntu 22.04 AMI (official Canonical)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_security_group" "ssh/http" {
+  name        = "allow-ssh"
+  description = "Allow SSH access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
+  security_groups = [
+    aws_security_group.ssh.name
+  ]
+
   tags = {
-    Name = "bank-terraform-vm"
+    Name = "flask-app"
   }
 }
